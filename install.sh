08f3f3fba209071d3445739cf68d56a28fcfb383
Ideilson Souza <ideilson.raise@gmail.com>
2024-04-10 02:50:49 -0300

    Correção do dir

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ sudo apt-get install -y supervisor
 # Cria um arquivo de configuração para o script Python no Supervisor
 sudo bash -c 'cat > /etc/supervisor/conf.d/socket_zdac.conf <<EOF
 [program:socket_zdac]
-command=/usr/bin/python3 /home/zabe/groupzabe/socket_zdac/main.py
-directory=/home/zabe/groupzabe/socket_zdac/
+command=/usr/bin/python3 /home/zabe/socket_zdac/main.py
+directory=/home/zabe/socket_zdac/
 autostart=true
 autorestart=true
 stderr_logfile=/var/log/socket_zdac.err.log
